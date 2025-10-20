5d7f7a184128fb81799910f4a133e01a690a24a5
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-10-20 11:09:06 +0900

    9章まで完了

---
+++ app/_actions/contact.ts
@@ -0,0 +1,83 @@
+"use server";
+
+function validateEmail(email: string) {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(String(email).toLowerCase());
+}
+
+export async function createContactData(_prevState: any, formData: FormData) {
+  const rawFormData = {
+    lastname: formData.get("lastname") as string,
+    firstname: formData.get("firstname") as string,
+    company: formData.get("company") as string,
+    email: formData.get("email") as string,
+    message: formData.get("message") as string,
+  };
+
+  if (!rawFormData.lastname) {
+    return { status: "error", message: "姓を入力してください" };
+  }
+
+  if (!rawFormData.firstname) {
+    return { status: "error", message: "名を入力してください" };
+  }
+
+  if (!rawFormData.company) {
+    return { status: "error", message: "会社名を入力してください" };
+  }
+
+  if (!rawFormData.email) {
+    return { status: "error", message: "メールアドレスを入力してください" };
+  }
+
+  if (!rawFormData.message) {
+    return { status: "error", message: "メッセージを入力してください。" };
+  }
+
+  const result = await fetch(
+    `https://api.hsforms.com/submissions/v3/integration/submit/${process.env.HUBSPORT_PORTAL_ID}/${process.env.HUBSPORT_FORM_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: [
+          {
+            obectTypeId: "0-1",
+            name: "lastname",
+            value: rawFormData.lastname,
+          },
+          {
+            obectTypeId: "0-1",
+            name: "firstname",
+            value: rawFormData.firstname,
+          },
+          {
+            obectTypeId: "0-1",
+            name: "company",
+            value: rawFormData.company,
+          },
+          {
+            obectTypeId: "0-1",
+            name: "email",
+            value: rawFormData.email,
+          },
+          {
+            obectTypeId: "0-1",
+            name: "message",
+            value: rawFormData.message,
+          },
+        ],
+      }),
+    }
+  );
+
+  try {
+    await result.json();
+  } catch (e) {
+    return { status: "error", message: "お問い合わせに失敗しました。" };
+  }
+
+  return { status: "success", message: "OK" };
+}
